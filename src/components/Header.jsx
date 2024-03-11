a15c2911bcab42b1d5b9942a980465d7e2093e8a
Sahil <ss6611786@gmail.com>
2024-03-11 15:04:25 +0530

    dark mode is working now

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,25 @@
-import React from 'react';
-import MenuItems from './MenuItems';
-import { AiFillHome } from 'react-icons/ai';
-import { BsFillInfoCircleFill } from 'react-icons/bs';
-import Link from 'next/link';
+import React from "react";
+import MenuItems from "./MenuItems";
+import { AiFillHome } from "react-icons/ai";
+import { BsFillInfoCircleFill } from "react-icons/bs";
+import Link from "next/link";
+import Darkmode from "./Darkmode";
 
 export default function Header() {
   return (
-      <div className='flex justify-between items-center p-3 max-w-6xl mx-auto'>
-          <div className='flex gap-4'>
-              <MenuItems title="home" address="/" Icon={AiFillHome} />
-              <MenuItems title="about" address="/about" Icon={BsFillInfoCircleFill} />
-          </div>
-          <Link href={'/'} className='flex gap-1 items-center'>
-              <span className='text-2xl font-bold bg-amber-500 rounded-lg px-2 py-1'>
-                  CineSphere
-              </span>
-             
-          </Link>
+    <div className="flex justify-between items-center p-3 max-w-6xl mx-auto">
+      <div className="flex gap-4">
+        <MenuItems title="home" address="/" Icon={AiFillHome} />
+        <MenuItems title="about" address="/about" Icon={BsFillInfoCircleFill} />
+      </div>
+      <div className="flex items-center gap-4">
+        <Darkmode />
+        <Link href={"/"} className="flex gap-1 items-center">
+          <span className="text-2xl font-bold bg-amber-500 rounded-lg px-2 py-1">
+            CineSphere
+          </span>
+        </Link>
+      </div>
     </div>
-  )
+  );
 }
